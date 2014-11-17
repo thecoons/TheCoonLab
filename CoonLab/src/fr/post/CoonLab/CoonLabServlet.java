5522d474f504806c761342e3bf27ea2458801a8a
thecoons <antobarth@gmail.com>
2014-11-17 14:00:29 +0100

    Fist Commit

---
+++ CoonLab/src/fr/post/CoonLab/CoonLabServlet.java
@@ -0,0 +1,13 @@
+package fr.post.CoonLab;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class CoonLabServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
